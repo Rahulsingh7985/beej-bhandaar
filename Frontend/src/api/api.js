9f5191052b9c9d722dbba55a0f884e8b5ba2390d
Rahul Singh patel <rahulsingh835789@gmail.com>
2025-12-21 20:30:55 +0530

    Comment out API configuration in api.js
    Comment out axios API configuration for future reference.

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
-  withCredentials: true
-});
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api", // backend URL
+//   withCredentials: true
+// });
 
-export default API;
+// export default API;
