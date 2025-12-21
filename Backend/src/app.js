6deb1a3bc8d53485ff33858dfc58cf54f6957cda
Rahul Singh patel <rahulsingh835789@gmail.com>
2025-12-21 19:54:16 +0530

    Fix console log syntax for server message

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -34,7 +34,7 @@ app.use("/api/v2/posts", postRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
 // Default route for testing
 app.get("/", (req, res) => {
